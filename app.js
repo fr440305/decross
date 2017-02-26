70d1ba2c1879cedb5ef2ced467b85cbfd876a14a
FireRain <fr440305@gmail.com>
2017-02-26 15:49:13 -0800

    app index - Add some basic function

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+function Log (txt) {
+	console.log(txt);
+}
 
+function Eventer () {
+
+}
+
+function Render () {
+	this.appui = document.getElementById("app");
+	this.appctx = this.appui.getContext("2d");
+	this.Rect('green', 0, 0, 100, 100);
+}
+
+Render.prototype.Rect = function (color, x0, y0, dx, dy) {
+	this.appctx.fillStyle = color;
+	this.appctx.fillRect(x0, y0, dx, dy);
+}
+
+function App () {
+	Log("asd");
+	this.render = new Render();
+}
+
+
+
+new App();
