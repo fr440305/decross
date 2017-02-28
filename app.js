bf2d47eaca7cc33323c2d0de18e0e1f641766aa7
FireRain <fr440305@gmail.com>
2017-02-27 23:10:35 -0800

    app - add file header and App.looper()

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-function Log (txt) {
-	console.log(txt);
+//author.name = 'FIRERAIN';
+//author.email = 'fr440305@gmail.com';
+//project.appjs.name = 'app.js';
+
+function Log (txt) {console.log(txt);}
+
+function Graph () {
+	this.nodes = [];
+	this.branches = [];
+}
+
+Graph.prototype.AddNode = function () {
 }
 
 function Eventer () {
-
 }
 
 function Render () {
@@ -17,9 +26,21 @@ Render.prototype.Rect = function (color, x0, y0, dx, dy) {
 	this.appctx.fillRect(x0, y0, dx, dy);
 }
 
+Render.prototype.Looper = function () {
+}
+
 function App () {
 	Log("asd");
+	this.stat = undefined;
 	this.render = new Render();
+	this.evneter = new Eventer();
+	this.frame_rate = 1; //fps
+	var Me = this;
+	setInterval(function(){Me.looper()}, 1000 / Me.frame_rate);
+}
+
+
+App.prototype.looper = function () {
 }
 
 
