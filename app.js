19754cc40e54341d703c0305d6010309c90a7144
FireRain <fr440305@gmail.com>
2017-02-28 20:09:51 -0800

    app - now render can draw the nodes in graph

--- app.js
+++ app.js
@@ -5,8 +5,9 @@
 
 function Log (txt) {console.log(txt);}
 
+//Graph::$
 function Graph () {
-	this.nodes = [];
+	this.nodes = [{x: 100, y: 100}];
 	this.branches = [];
 }
 
@@ -16,12 +17,26 @@ Graph.prototype.LoadLevel = function (level_json) {
 Graph.prototype.AddNode = function () {
 }
 
+Graph.prototype.Get = function (what) {
+	return {
+		"nodes": this.nodes // need to change it to deep_copy.
+	}[what];
+}
+
+//Eventer::$
 function Eventer () {
+	this.touch_point = {x: undefined, y: undefined};
 	this.appui = document.getElementById("app");
 	//this.stat = {};
 	this.bind();
 }
 
+//Eventer::Get
+Eventer.prototype.Get = function (what) {
+	return { "touch-point": {x: this.touch_point.x, y:this.touch_point.y} }[what];
+}
+
+//Eventer::bind
 Eventer.prototype.bind = function () {
 	this.appui.addEventListener("touchend", function (e) {
 		e.preventDefault();
@@ -45,12 +60,16 @@ Render.prototype.Rect = function (color, x0, y0, dx, dy) {
 	this.appctx.fillRect(x0, y0, dx, dy);
 }
 
-Render.prototype.Looper = function () {
+Render.prototype.Looper = function (nodes) {
+	for (var i = 0; i < nodes.length; i++) {
+		this.Rect("black", nodes[i].x - 25, nodes[i].y - 25, 50, 50);
+	}
 }
 
 function App () {
 	Log("asd");
 	this.stat = undefined;
+	this.graph = new Graph();
 	this.render = new Render();
 	this.evneter = new Eventer();
 	this.frame_rate = 1; //fps
@@ -59,6 +78,7 @@ function App () {
 }
 
 App.prototype.looper = function () {
+	this.render.Looper( this.graph.Get("nodes") );
 }
 
 new App();
