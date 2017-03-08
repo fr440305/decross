bb1de89dc431637e83d0f8f61e1e9bf93944968b
FireRain <fr440305@gmail.com>
2017-03-07 21:27:35 -0800

    server - change print

--- server.go
+++ server.go
@@ -4,6 +4,6 @@ import "fmt"
 import "net/http"
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println("http://127.0.0.1:9999/[index.html]")
 	http.ListenAndServe(":9999", http.FileServer(http.Dir(".")))
 }
