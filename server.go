e0b9e9cf7b3a56164fccb042fdc62e762bd76e82
FireRain <fr440305@gmail.com>
2017-03-07 21:22:31 -0800

    server - clean & port:9999

--- server.go
+++ server.go
@@ -5,6 +5,5 @@ import "net/http"
 
 func main() {
 	fmt.Println("vim-go")
-	http.Handle("/", http.FileServer(http.Dir(".")))
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":9999", http.FileServer(http.Dir(".")))
 }
